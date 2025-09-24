fe316e0c671d7824f58e8befc54b8b6b820ff346
atharvakaplay123 <atharvakaplay@gmail.com>
2025-09-24 12:40:27 +0530

    cors added

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Load trained model
 model = joblib.load("crop_model.pkl")
